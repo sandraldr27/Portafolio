04f0675cc6d0fb99ea47270157278a346a442d44
sandraldr27 <sandralilder@gmail.com>
2023-11-19 20:11:03 +0100

    The app-routing-module is created and navigation routes are added to the application

---
+++ src/app/app-routing/app-routing.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+//import { CommonModule } from '@angular/common';
+
+//Rutas//
+import { Routes, RouterModule} from '@angular/router';
+
+//Componentes//
+import { PortafolioComponent } from '../pages/portafolio/portafolio.component';
+import { AboutComponent } from '../pages/about/about.component';
+import { ItemComponent } from '../pages/item/item.component';
+
+
+const app_routes: Routes = [  //Declaro mi constante (Arreglo) para las rutas. Aquí le indico a mi código a dónde quiero navegar dependiendo de cada ruta escogida//
+    { path: '', component: PortafolioComponent}, //El portafolio component es la página principal//
+    { path: 'about', component: AboutComponent },
+    { path: 'item', component: ItemComponent },
+    { path: '**', pathMatch: 'full', redirectTo: '' }  //Si se escribe cualquier cosa como una ruta, el navegador redigirá a la página principal o home. También se puede indicar colocando **. En este caso se está redireccionando a '' que es el portafolio//
+];
+
+@NgModule({
+  declarations: [],
+  imports: [
+    //CommonModule,
+    RouterModule.forRoot(app_routes)
+  ],
+  exports: [
+    RouterModule
+  ]
+})
+export class AppRoutingModule { }
